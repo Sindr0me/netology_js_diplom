691382da062e237344c42f0dea2e675ea8169451
Sindr0me <kupriyanov.vo@gmail.com>
2018-05-13 18:42:02 +0300

    adeed class Player

--- js-game-master/game.js
+++ js-game-master/game.js
@@ -159,6 +159,15 @@ class Level {
   }
 }
 
+class Player extends Actor {
+  constructor(pos = new Vector(0, 0)) {
+    super(pos.plus(new Vector(0, -0.5)), new Vector(0.8, 1.5));
+  }
+
+  get type() {
+    return 'player';
+  }
+}
 
 // -------------start game-------------
 const grid = [
